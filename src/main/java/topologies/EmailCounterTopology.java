57265005181091018573f1d82f356eedb0b521c4
Rhys Fernandes <rkfernandes@gmail.com>
2017-07-31 23:43:08 -0400

    EmailCounterTopology in it's own file

---
+++ src/main/java/topologies/EmailCounterTopology.java
@@ -0,0 +1,27 @@
+package topologies;
+
+import bolts.EmailCounter;
+import bolts.EmailExtractor;
+import org.apache.storm.generated.StormTopology;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
+import spouts.CommitFeedListener;
+
+public class EmailCounterTopology {
+    public static StormTopology build() {
+        TopologyBuilder topologyBuilder = new TopologyBuilder();
+
+        topologyBuilder
+                .setSpout("commit-feed-listener", new CommitFeedListener());
+
+        topologyBuilder
+                .setBolt("email-extractor", new EmailExtractor())
+                .shuffleGrouping("commit-feed-listener");
+
+        topologyBuilder
+                .setBolt("email-counter", new EmailCounter())
+                .fieldsGrouping("email-extractor", new Fields("email"));
+
+        return topologyBuilder.createTopology();
+    }
+}
